31fb51b3a59c83d2daceb69536d35a9848dcfde6
soso <yizao0928@gmail.com>
2023-02-19 18:11:53 +0800

    first commit

---
+++ src/Card.tsx
@@ -0,0 +1,10 @@
+import { CardContainer } from "./styles";
+
+type CardProps = {
+    id: string
+    text: string
+}
+
+export const Card = ({ text }: CardProps) => {
+    return <CardContainer>{text}</CardContainer>
+}
